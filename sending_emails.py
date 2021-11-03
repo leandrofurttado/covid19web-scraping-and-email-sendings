e12fca82f71454768df5981f7bb7d8ee0bfff1e9
Leandro Furtado <69984719+leandrofurttado@users.noreply.github.com>
2021-11-03 13:13:36 -0300

    Update sending_emails.py

--- sending_emails.py
+++ sending_emails.py
@@ -1,22 +1,15 @@
-'''
-
-CONTA:
-pythonprogrammingleandro02@outlook.com
-123456789Saco
-
-'''
 import smtplib
 from email.message import EmailMessage
 
 # Configurando o email e senha:
-EMAIL = 'opala.gyb02@gmail.com'
-PASSWORD = 'Leanlv900'
+EMAIL = 'seu_email_aqui@email.com'
+PASSWORD = 'sua_senha_aqui'
 
 '''# Criando o email que será enviado:
 email_message = EmailMessage()
 email_message['Subject'] = 'TESTE DE SUBJECT'
-email_message['From'] = 'opala.gyb02@gmail.com'
-email_message['To'] = 'leandrofx2001@hotmail.com'
+email_message['From'] = 'email_remetente@email.com'
+email_message['To'] = 'email_destinatário@email.com'
 email_message.set_content('ESSE É O TESTE E APARENTEMENTE DEU TUDO CERTO!')'''
 
 # ENVIANDO O EMAIL:
@@ -24,9 +17,9 @@ email_message.set_content('ESSE É O TESTE E APARENTEMENTE DEU TUDO CERTO!')'''
 
 def envia_email(msg):
     email_message = EmailMessage()
-    email_message['Subject'] = 'ATUALIZAÇÃO COVID-19 SCRIPT'
-    email_message['From'] = 'opala.gyb02@gmail.com'
-    email_message['To'] = 'leandrofx2001@hotmail.com'
+    email_message['Subject'] = 'ATUALIZAÇÃO COVID-19 SCRIPT' #TÍTULO DO EMAIL
+    email_message['From'] = 'email_remetente@email.com'
+    email_message['To'] = 'email_destinatário@email.com'
     email_message.set_content(msg)
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(EMAIL, PASSWORD)
